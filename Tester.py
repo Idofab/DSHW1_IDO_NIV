a3e8faf6ac1443f152c72a8adb15e6eca49c6811
idofab <idofab@gmail.com>
2022-12-08 22:15:08 +0200

    0812 22:15

--- Tester.py
+++ Tester.py
@@ -2,14 +2,11 @@ import avl_template_new as avl
 
 
 avl_list = ['A', '-2', '3', '4', '20', '-13', 'abc']
-avl_nodes = []
-for i in range(len(avl_list)):
-	avl_nodes.append(avl.AVLNode((i, avl_list[i])))
 
 avl_tree = avl.AVLTreeList()
 
 avl_tree.insert(0, avl_list[0])
-avl_list = ['-2', '3', '4', '20', '-13', 'abc']
+avl_list = ['B', 'c', 'd', 'e', 'f', 'g']
 
 for i, val in enumerate(avl_list):
 	avl_tree.insert(i, val)
