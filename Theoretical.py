92e37e25bae9f1bca0dd2e692bdeb187615077fc
idofab <idofab@gmail.com>
2022-12-24 12:50:09 +0200

    2412 12:50

--- Theoretical.py
+++ Theoretical.py
@@ -2,11 +2,11 @@ import avl_template_new as avl
 import random
 import time
 import datetime
-from collections import deque
+import LinkedList as ll
 
 def q1():
 	ans_list = []
-	for i in [0, 1, 6]:
+	for i in [6]:
 		print(f'================i = {i + 1}===============')
 		print(f'Starting time {datetime.datetime.now()}')
 		start = time.time()
@@ -40,16 +40,16 @@ def q1():
 			if((j/K * 100) % 1 == 0):
 				print(f'i = {i+1}, c1 - {j/(K//2)*100}% - {datetime.datetime.now()}')
 
-		for j in range(K // 4):
-			del_ins = random.randint(0, 1)
-			if del_ins == 1:
-				rand_ind = random.randint(0, c_tree.length())
-				temp = c_tree.insert(rand_ind, j)
-			else:
-				rand_ind = random.randint(0, c_tree.length() - 1)
-				temp = c_tree.delete(rand_ind)
+		for j in range(K // 8):
+
+			rand_ind = random.randint(0, c_tree.length())
+			temp = c_tree.insert(rand_ind, j)
+			c_rotation_cnt += temp
+
+			rand_ind = random.randint(0, c_tree.length() - 1)
+			temp = c_tree.delete(rand_ind)
 			if((j/K * 100) % 1 == 0):
-				print(f'i = {i+1}, c2 - {j/(K//4)*100}% - {datetime.datetime.now()}')
+				print(f'i = {i+1}, c2 - {j/(K//8)*100}% - {datetime.datetime.now()}')
 			c_rotation_cnt += temp
 		
 		ans_list.append((a_rotation_cnt, b_rotation_cnt, c_rotation_cnt))
@@ -61,6 +61,16 @@ def q1():
 def q2():
 	for i in range(10):
 		K = 1500 * i
+		linked_list = ll.LinkedList()
+		theo_list = []
+		
+		avl_tree = avl.AVLTreeList()
+		avl_start = time.time()
+		for j in range(K):
+			avl_tree.insert(0, j)
+		
+		avl_end = time.time()
+		print(f"Avl tree time: {avl_end - avl_start}")
 
 
 	return
