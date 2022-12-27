ba2baf3de68acf840a62466622c09adcea4cbd37
idofab <idofab@gmail.com>
2022-12-27 17:33:25 +0200

    2712 17:30

--- LinkedList.py
+++ LinkedList.py
@@ -19,7 +19,35 @@ class LinkedList:
         nodes.append("None")
         return " -> ".join(nodes)
     
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
     def add_first(self, node):
         node.next = self.head
         self.head = node
+    
+    def add_last(self, node):
+        if self.head is None:
+            self.head = node
+            return
+        for current_node in self:
+            pass
+        current_node.next = node
+    
+    def add_index(self, node, i):
+        if (i==0):
+            self.add_first(node)
+        j = 0
+        
+        for ind_node in self:
+            j+=1
+            if j == i:
+                break
+        node.next = ind_node.next
+        ind_node.next = node
+
+            
 
